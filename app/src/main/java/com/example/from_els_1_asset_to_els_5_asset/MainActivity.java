5f583c0cd8f93fd041b5c3436082696d251e78bc
yonghwadragon <yongyonghwa@naver.com>
2024-03-11 00:02:50 +0900

    The AndroidManifest.xml file defines the information, features, components, and lifecycle of an Android app.

--- app/src/main/java/com/example/from_els_1_asset_to_els_5_asset/MainActivity.java
+++ app/src/main/java/com/example/from_els_1_asset_to_els_5_asset/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.from_els_1_asset_to_els_5_asset;
 
-public class MainActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 }
